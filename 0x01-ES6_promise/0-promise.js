cb0db18da2f978fdac44ef5c39522837ba2d809d
oussamaNAJIH <oussamanajih6@gmail.com>
2024-04-09 23:36:27 +0200

    Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    resolve(true);
-    reject(new Error('An error occurred while fetching data from the API'));
-  });
+  return new Promise(() => {});
 }
