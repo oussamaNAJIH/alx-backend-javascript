92a6793baa92cb6768cac5fcd9eac9a039b5e8a4
oussamaNAJIH <oussamanajih6@gmail.com>
2024-04-09 23:08:36 +0000

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default function handleResponseFromAPI(promise) {
   return promise.then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'Success' };
-  }).catch(() => return new Error());
+  }).catch(() => new Error());
 }
