79fe91c0c5944c9133704a99de5ff68f231d27c8
oussamaNAJIH <oussamanajih6@gmail.com>
2024-04-11 15:55:19 +0000

    Primitive - Holberton Class

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,22 +1,22 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-        this._size = size;
-        this._location = location;
-    }
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
 
-    valueOf() {
-        return this._size;
-    }
+  valueOf() {
+    return this._size;
+  }
 
-    toString() {
-        return this._location;
-    }
+  toString() {
+    return this._location;
+  }
 
-    get size() {
-        return this._size;
-    }
+  get size() {
+    return this._size;
+  }
 
-    get location() {
-        return this._location;
-    }
+  get location() {
+    return this._location;
+  }
 }
