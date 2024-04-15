b9ccb454b7179cd81985aea253d527a82fa09d01
oussamaNAJIH <oussamanajih6@gmail.com>
2024-04-15 17:46:41 +0200

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -3,6 +3,6 @@ export default function getListStudents() {
   const getStudent = (id, firstName, location) => ({ id, firstName, location });
   myArray.push(getStudent(1, 'Guillaume', 'San Francisco'));
   myArray.push(getStudent(2, 'James', 'Columbia'));
-  myArray.push(getStudent(3, 'Serena', 'San Francisco'));
+  myArray.push(getStudent(5, 'Serena', 'San Francisco'));
   return myArray;
 }
