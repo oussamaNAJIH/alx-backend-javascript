77bd43694425c1bc94bee8b31e7a0eb80952e406
oussamaNAJIH <oussamanajih6@gmail.com>
2024-04-16 00:47:56 +0200

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,15 @@
 export default function cleanSet(set, startString) {
-  
+  if (!set || !startString) {
+    return '';
+  }
+  let result = '';
+  for (const element of set) {
+    if (element.startsWith(startString)) {
+      result = result.concat(element.substring(startString.length), '-');
+    }
+  }
+  if (result.length > 1) {
+    result = result.substring(0, result.length - 1);
+  }
+  return result;
 }
