533145ae3512dcf8c88dc0c7eea22d9affbb2612
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-03 17:53:50 +0200

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,11 @@
 console.log('Welcome to Holberton School, what is your name?');
+
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString()}`);
+  const name = data.toString().trim();
+
+  console.log(`Your name is: ${name}`);
 });
+
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
