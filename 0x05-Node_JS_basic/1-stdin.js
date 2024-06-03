2b21472efb55e07ee9d26ad8e3850420401d123f
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-03 18:10:53 +0200

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,8 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
+    const name = chunk.toString().trim();
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
