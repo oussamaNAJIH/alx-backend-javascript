369a0be3dcfe0d805d90d78c48caf96d3f3a3b37
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-04 16:54:18 +0200

    Create a more complex HTTP server using Nodes HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
-const path = process.argv[1];
+const path = process.argv[2]; // Corrected argument index
 
 const app = http.createServer((req, res) => {
     if (req.url === '/') {
@@ -8,11 +8,24 @@ const app = http.createServer((req, res) => {
         res.end('Hello Holberton School!');
     }
     if (req.url === '/students') {
-        res.write('This is the list of our students');
-        countStudents(path);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write('This is the list of our students\n');
+        // Call countStudents and handle the Promise
+        countStudents(path)
+            .then(() => {
+                console.log('Database processed successfully.');
+                // At this point, the database processing is done
+                // You should send the response back to the client here
+                res.end(); // End the response
+            })
+            .catch((err) => {
+                console.error('Error processing database:', err.message);
+                res.end(); // End the response in case of error
+            });
     }
 });
 
 app.listen(1245);
 
 module.exports = app;
+
