c45c556d78252105ff85163adde31b25ad4a59d8
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-04 16:35:39 +0200

    rrr

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,6 @@
 const http = require('http');
 
 /**
- * HTTP server that responds with "Hello Holberton School!" to any endpoint.
- * @module 4-http
- */
-
-/**
  * Callback function that handles incoming HTTP requests.
  * @param {http.IncomingMessage} req The incoming HTTP request.
  * @param {http.ServerResponse} res The server response that will be sent back to the client.
