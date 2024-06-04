0559a4dddea36f47831fb9e8eb345a36f2eb64cc
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-04 12:30:54 +0200

    simple app

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,8 @@
 const http = require('http');
 
 const app = http.createServer((req, resp) => {
-    if (req) {
-        resp.writeHead(200, { 'Content-Type': 'text/plain' });
-        resp.end('Hello Holberton School!');
-    }
-    
+    resp.writeHead(200, { 'Content-Type': 'text/plain' });
+    resp.end('Hello Holberton School!');
 });
 app.listen(1245);
 module.exports = app;
