d918962c306cc8f4756514c51b2ddf6b24c0877e
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-04 16:38:23 +0200

    rrr

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,5 @@
 const http = require('http');
 
-/**
- * Callback function that handles incoming HTTP requests.
- * @param {http.IncomingMessage} req The incoming HTTP request.
- * @param {http.ServerResponse} res The server response that will be sent back to the client.
- */
-
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
