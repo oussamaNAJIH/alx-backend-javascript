1824689318b74b4f4bb3457e71a096b35c5aa357
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-05 23:22:21 +0200

    async test

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,14 +2,11 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 const expect = require("chai").expect;
 
 describe('getPaymentTokenFromAPI', () => {
-    it('should return a successful response object when success is true', (done) => {
+    it('should return a successful response object when success is true', () => {
         getPaymentTokenFromAPI(true)
             .then((response) => {
-                expect(response).toEqual({ data: 'Successful response from the API' });
+                expect(response).to.deep.equal({ data: 'Successful response from the API' });
                 done();
             })
-            .catch((error) => {
-                done(error);
-            });
     });
 });
