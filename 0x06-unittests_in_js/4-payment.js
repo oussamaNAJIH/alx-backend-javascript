40896c085e08120172a58636a5a9bd3b01d4d176
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-05 23:58:33 +0200

    stub

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,8 +1,7 @@
 const Utils = require('./utils');
 
-const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${totalCost}`);
-};
-
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${total}`);
+}
 module.exports = sendPaymentRequestToApi;
