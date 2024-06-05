802d1d7b808db7bcf10c564fd991008de80d6047
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-05 23:20:05 +0200

    async test

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,11 +1,7 @@
-function getPaymentTokenFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({ data: 'Successful response from the API' });
-        } else {
-            reject(new Error('API request failed'));
-        }
-    });
-}
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  if (success) {
+    resolve({data: 'Successful response from the API'});
+  }
+});
 
 module.exports = getPaymentTokenFromAPI;
