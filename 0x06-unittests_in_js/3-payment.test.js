aff851686c05c87165b1ba6656d8b9cd5bc36c8f
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-05 21:40:19 +0200

    spy

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,25 +2,11 @@ const sinon = require("sinon");
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
-describe('make sure the same math is used', function() {
-    // let calculateNumberSpy;
-
-    // beforeEach(function() {
-    //     calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-    // });
-
-    // afterEach(function() {
-    //     calculateNumberSpy.restore();
-    // });
-
-    // it('should call Utils.calculateNumber with correct arguments', function() {
-    //     sendPaymentRequestToApi(100, 20);
-    //     sinon.assert.calledOnceWithExactly(calculateNumberSpy, 'SUM', 100, 20);
-    // });
+describe("make sure the same math is used", function() {
     it("checking the execution of calculateNumber once", function() {
-        const spy = sinon.spy(Utils, "calculateNumber");
-        sendPaymentRequestToApi(100, 20);
-        assert(spy.calledOnce);
-        spy.restore();
-    })
+	const spy = sinon.spy(Utils, "calculateNumber");
+	sendPaymentRequestToApi(100, 20);
+	assert(spy.calledOnce);
+	spy.restore();
+    });
 });
