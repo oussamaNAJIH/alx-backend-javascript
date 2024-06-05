98beca27c3ef026184327a02286a5ae9f532bc5b
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-05 23:41:30 +0200

    Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,16 +1,14 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api');
 
-describe('Index page', () => {
-    it('should return status code 200 and the correct message', (done) => {
-        request(app)
-            .get('/')
-            .end((err, res) => {
-                expect(res.statusCode).to.be.equal(200);
-                expect(res.text).to.be.equal('Welcome to the payment system');
-                done();
-            });
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
     });
+  });
 });
-
