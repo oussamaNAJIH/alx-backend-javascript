4c8f97b2a269ecb940e2dff6bc8bafcf8b106dad
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-05 22:10:37 +0200

    stub

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -2,21 +2,14 @@ const sinon = require("sinon");
 const sendPaymentRequestToApi = require('./3-payment');
 const { describe, it } = require("mocha");
 const Utils = require('./utils');
-const expect = require("chai").expect;
+const assert = require("assert");
 
 describe("checking the same math is used", function() {
     it("staubing calculateNumber", function() {
         const consoleLogSpy = sinon.spy(console, "log");
         const calculateNumberStub = sinon.stub(Utils, "calculateNumber").returns(10);
         sendPaymentRequestToApi(100, 20);
-        
-        expect(consoleLogSpy.calledOnce).to.be.true;
-        expect(consoleLogSpy.calledWithExactly("The total is: 10")).to.be.true;
-
-        expect(calculateNumberStub.calledOnce).to.be.true;
-        expect(calculateNumberStub.calledWithExactly("SUM", 100, 20)).to.be.true;
-
-        consoleLogSpy.restore();
-        calculateNumberStub.restore();
+		assert(consoleLogSpy.withArgs("The total is: 10").calledOnce);
+		assert(calculateNumberStub.withArgs("SUM", 100, 20).calledOnce);
     });
 });
