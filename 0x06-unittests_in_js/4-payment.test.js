1aa27f8ae726ffed3e0d8c7ed831e3f22da379e0
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-05 23:57:35 +0200

    stub

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,22 +1,20 @@
-const sinon = require("sinon");
-const sendPaymentRequestToApi = require('./4-payment');
-const { describe, it } = require("mocha");
+const sinon = require('sinon');
 const Utils = require('./utils');
-const expect = require("chai").expect;
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
 
-describe("checking the same math is used", function() {
-    it("staubing calculateNumber", function() {
-        const consoleLogSpy = sinon.spy(console, "log");
-        const calculateNumberStub = sinon.stub(Utils, "calculateNumber").returns(10);
-        sendPaymentRequestToApi(100, 20);
-        
-        expect(consoleLogSpy.calledOnce).to.be.true;
-        expect(consoleLogSpy.calledWithExactly("The total is: 10")).to.be.true;
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
+    const bigBrother = sinon.spy(console);
+    const dummy = sinon.stub(Utils, 'calculateNumber');
 
-        expect(calculateNumberStub.calledOnce).to.be.true;
-        expect(calculateNumberStub.calledWithExactly("SUM", 100, 20)).to.be.true;
-
-        consoleLogSpy.restore();
-        calculateNumberStub.restore();
-    });
+    dummy.returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(dummy.callCount).to.be.equal(1);
+    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
+    expect(bigBrother.log.callCount).to.be.equal(1);
+    dummy.restore();
+    bigBrother.log.restore();
+  });
 });
