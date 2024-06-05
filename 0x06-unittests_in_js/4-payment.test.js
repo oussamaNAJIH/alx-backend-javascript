9abbdcfcc40c5d185bec501be6c6ebabdb80ea29
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-05 22:13:48 +0200

    stub

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,16 +1,18 @@
-const {describe, it} = require("mocha");
 const sinon = require("sinon");
-const sendPaymentRequestToApi = require("./4-payment");
-const Utils = require("./utils");
-const assert = require("assert");
+const sendPaymentRequestToApi = require('./3-payment');
+const { describe, it } = require("mocha");
+const Utils = require('./utils');
 
-describe("sendPaymentRequestToApi", function() {
-    it("check that Utils.calculateNumber is stubbed", function() {
-	const spy = sinon.spy(console, "log");
-	const stub = sinon.stub(Utils, "calculateNumber").returns(10);
-	sendPaymentRequestToApi(100, 20);
-
-	assert(spy.withArgs("The total is: 10").calledOnce);
-	assert(stub.withArgs("SUM", 100, 20).calledOnce);
+describe("make sure the same math is used", function() {
+    it("checking the execution of calculateNumber once", function() {
+        const consoleLogSpy = sinon.spy(console, "log");
+        const calculateNumberStub = sinon.stub(Utils, "calculateNumber").returns(10);
+        sendPaymentRequestToApi(100, 20);
+        sinon.assert.calledOnce(consoleLogSpy);
+        sinon.assert.calledWithExactly(consoleLogSpy, "The total is: 10");
+        sinon.assert.calledOnce(calculateNumberStub);
+        sinon.assert.calledWithExactly(calculateNumberStub, "SUM", 100, 20);
+        consoleLogSpy.restore();
+        calculateNumberStub.restore();
     });
 });
