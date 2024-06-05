6971cf79f7945d00f10fa21ba9af22e6a723978e
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-05 21:32:04 +0200

    spy

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -3,24 +3,18 @@ const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
 describe('make sure the same math is used', function() {
-    // let calculateNumberSpy;
+    let calculateNumberSpy;
 
-    // beforeEach(function() {
-    //     calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-    // });
+    beforeEach(function() {
+        calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    });
 
-    // afterEach(function() {
-    //     calculateNumberSpy.restore();
-    // });
+    afterEach(function() {
+        calculateNumberSpy.restore();
+    });
 
-    // it('should call Utils.calculateNumber with correct arguments', function() {
-    //     sendPaymentRequestToApi(100, 20);
-    //     sinon.assert.calledOnceWithExactly(calculateNumberSpy, 'SUM', 100, 20);
-    // });
-    it("checking the execution of calculateNumber once", function() {
-        let spy = sinon.spy(Utils, 'calculateNumber'); // Corrected 'utils' to 'Utils'
+    it('should call Utils.calculateNumber with correct arguments', function() {
         sendPaymentRequestToApi(100, 20);
-        assert(spy.calledOnce);
-        spy.restore();
-    })
+        sinon.assert.calledOnceWithExactly(calculateNumberSpy, 'SUM', 100, 20);
+    });
 });
