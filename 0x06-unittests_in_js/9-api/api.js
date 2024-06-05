edf627ca2397cdde700a1158460917270c9147ad
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-06 00:20:04 +0200

    api

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -5,6 +5,12 @@ app.get('/', (req, resp) => {
   resp.send('Welcome to the payment system');
 });
 
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const cartId = req.params.id;
+  res.send(`Payment methods for cart ${cartId}`);
+});
+
+
 app.listen(7865, () => {
     console.log('API available on localhost port 7865');
 });
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,11 +2,32 @@ const request = require('request');
 const expect = require("chai").expect;
 
 describe('API Integration Testing', function() {
+  // Test for the root endpoint
   it('should return the correct response for GET /', function(done) {
     request.get('http://localhost:7865/', function(error, response, responseBody) {
       expect(response.statusCode).to.equal(200);
       expect(responseBody).to.equal('Welcome to the payment system');
       done();
+    });
+  });
+
+  // Test suite for the cart page
+  describe('GET /cart/:id', function() {
+    // Correct status code when :id is a number
+    it('should return 200 and correct response for a valid cart id', function(done) {
+      request.get('http://localhost:7865/cart/123', function(error, response, responseBody) {
+        expect(response.statusCode).to.equal(200);
+        expect(responseBody).to.equal('Payment methods for cart 123');
+        done();
+      });
+    });
+
+    // Correct status code when :id is NOT a number
+    it('should return 404 for an invalid cart id', function(done) {
+      request.get('http://localhost:7865/cart/abc', function(error, response, responseBody) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
     });
   });
 });
